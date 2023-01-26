0029ea2af4c9972f79565f2f40bda43bc5896b2a
boscha1 <bosch.anthony7@gmail.com>
2023-01-26 10:53:41 -0500

    separating controller into separate package

--- app.py
+++ app.py
@@ -1,55 +1,10 @@
-from crypt import methods
-from marshmallow import fields, ValidationError
-from flask import Flask, Response, jsonify, request
-from bson.json_util import dumps
-from requests import request
-from model.StockDTO import StockDTO
-from service.StockService import StockService
-from flask_restful import Api, Resource, reqparse
+from flask import Flask
+from controller.StockController import stock_blueprint
+
 
 app = Flask(__name__)
-api = Api(app)
+app.register_blueprint(stock_blueprint)
 
-@api.resource('/stock/<string:symbol>', '/stock')
-class StockController(Resource):
-    def __init__(self):
-        self.stock_service = StockService()
-        self.reqparse = reqparse.RequestParser()
-        self.reqparse.add_argument('symbol', type=str, required=True, help='No symbol provided', location='json')
-        self.reqparse.add_argument('name', type=str, required=True, help='No name provided', location='json')
-        self.reqparse.add_argument('price', type=float, required=True, help='No price provided', location='json')
-        super().__init__()
-
-
-    def get(self):
-        stocks = self.stock_service.get_all_stocks()
-        return create_response(stocks, 200)
-    
-    
-    def post(self):
-        try:
-            stock_dto = StockDTO().load(self.reqparse.parse_args())
-            exists = self.stock_service.get_stock_by_symbol(stock_dto['symbol'])
-            if exists:
-                return create_response("A stock with " + stock_dto['symbol'] + " already exists", 400)
-            self.stock_service.insert_stock(stock_dto)
-            stock_dto.pop("_id", None)
-            return create_response(stock_dto, 201)
-        except ValidationError as err:
-            return err.messages, 400
-        
-        
-    def delete(self):
-        self.stock_service.delete_all()
-        return create_response({"message": "deleted"}, 204)
-
-        
-def create_response(data, status_code):
-    return Response(
-        response=dumps(data),
-        status=status_code,
-        mimetype="application/json"
-    )
 
 if __name__ == '__main__':
     app.run()
---
+++ controller/__init__.py
@@ -0,0 +1,1 @@
+from .StockController import stock_blueprint
